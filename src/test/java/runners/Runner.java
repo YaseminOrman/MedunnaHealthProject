ac2443a556a0b4e3c1afa822afc48ecea877ebb1
YaseminOrman <jazmintunc@gmail.com>
2023-03-12 20:21:49 -0500

    new file

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -4,19 +4,19 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
-//        plugin = {
-//                "pretty",
-//                "html:target/default-cucumber-reports.html",
-//                "json:target/json-reports/cucumber.json",
-//                "junit:target/xml-report/cucumber.xml",
-//                "rerun:target/failedRerun.txt",
-//                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-//        },
-//        monochrome=true,
+        plugin = {
+                "pretty",
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+                "rerun:target/failedRerun.txt",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+        monochrome=false,
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         dryRun = false,
-        tags = "@US06_DB_Testing"
+        tags = "@US29_DB_Testing"
 )
 public class Runner {
 }
--- src/test/java/stepdefinitions/DB/US_06_DBStepDefinition.java
+++ src/test/java/stepdefinitions/DB/US_06_DBStepDefinition.java
@@ -42,6 +42,4 @@ public class US_06_DBStepDefinition {
       DBUtils.closeConnection();
     }
 
-
-
 }
